f3acdb8f8121c2a51b33ccf2fed1478e2a9f2be7
sambento7 <diogoamorim135@outlook.com>
2025-06-18 18:40:09 +0100

    mais testes

--- tests/test_protocol.py
+++ tests/test_protocol.py
@@ -55,46 +55,259 @@ def test_decode_non_terminated_iid():
         pass  # Expected behavior
 
 TAG = b'kdk847ufh84jg87g\0'
+INVALID_TAG = b'badtag\0'
 
 # Componentes base
 msg_type_get = b"G"
 msg_type_set = b"S"
+invalid_msg_type = b"X"
 
 timestamp = (
     b"T\0" + b"7\0" +
-    b"01\0" + b"01\0" + b"2024\0" + b"12\0" +
-    b"00\0" + b"00\0" + b"000\0"
+    b"01\0" + b"01\0" + b"2024\0" + 
+    b"12\0" + b"00\0" + b"00\0" + b"000\0"
+)
+
+invalid_timestamp_type = (
+    b"I\0" + b"7\0" +
+    b"01\0" + b"01\0" + b"2024\0" +
+    b"12\0" + b"00\0" + b"00\0" + b"00\0"
+)
+invalid_timestamp_size = (
+    b"T\0" + b"5\0" +
+    b"01\0" + b"01\0" + b"2024\0" +
+    b"12\0" + b"00\0" + b"00\0" + b"00\0"
+)
+invalid_timestamp_component = (
+    b"T\0" + b"7\0" +
+    b"aa\0" + b"01\0" + b"2024\0" +
+    b"12\0" + b"00\0" + b"00\0" + b"00\0"
+)
+invalid_timestamp_date_format = (
+    b"T\0" + b"7\0" +
+    b"01\0" + b"01\0" + b"2024\0" +
+    b"12\0" + b"00\0" + b"00\0" + b"00\0"
 )
 
 message_id = b"abcdefgh12345678\0"
+invalid_msg_id = b"shortid\0"
+
 iid_section = b"1\0" + b"D\0" + b"2\0" + b"1\0" + b"1\0"
+invalid_iid_section_size = b"a\0" + b"D\0" + b"2\0" + b"1\0"
+invalid_iid_section_component_type = b"1\0" + b"I\0" + b"2\0" + b"1\0" + b"1\0"
+invalid_iid_section_component_size = b"1\0" + b"D\0" + b"1\0" + b"1\0" + b"1\0"
+invalid_iid_section_component_value = b"1\0" + b"D\0" + b"2\0" + b"a\0" + b"1\0"
+
 value_empty = b"0\0"
 value_valid_i = b"1\0" + b"I\0" + b"1\0" + b"42\0"
 value_valid_s = b"1\0" + b"S\0" + b"1\0" + b"ON\0"
 value_valid_t = b"1\0" + b"T\0" + b"7\0" + b"01\0" + b"01\0" + b"2024\0" + b"12\0" + b"00\0" + b"00\0" + b"000\0"
-error_list_empty = b"0\0"
+invalid_value_more_than_one = b"2\0" + b"I\0" + b"1\0" + b"42\0" + b"I\0" + b"1\0" + b"43\0"
+invalid_value_size = b"a\0" + b"I\0" + b"1\0" + b"42\0"
+invalid_value_component_type = b"1\0" + b"D\0" + b"1\0" + b"42\0"
+invalid_value_component_size = b"1\0" + b"I\0" + b"a\0" + b"42\0"
+invalid_value_i_componente_size = b"1\0" + b"I\0" + b"2\0" + b"42\0"
+invalid_value_s_componente_size = b"1\0" + b"S\0" + b"2\0" + b"42\0"
+invalid_value_t_componente_size =  b"1\0" + b"T\0" + b"5\0" + b"2024\0" + b"12\0" + b"00\0" + b"00\0" + b"000\0"
+invalid_value_component_value = b"1\0" + b"I\0" + b"1\0" + b"a\0"
+invalid_value_component_t_value = (
+    b"1\0" + b"T\0" + b"7\0" +
+    b"01\0" + b"01\0" + b"2024\0" +
+    b"12\0" + b"00\0" + b"00\0" + b"00\0"
+)
 
-def make_message(msg_type, values):
-    return TAG + msg_type + timestamp + message_id + iid_section + values + error_list_empty
+error_list_empty = b"0\0"
+invalid_error_list_size = b"a\0"
+unsupported_error_list_size = b"2\0"
+
+######################
+# TESTES GERAIS
+######################
+
+def test_valid_get():
+    p = Protocol()
+    msg = TAG + msg_type_get + timestamp + message_id + iid_section + value_empty + error_list_empty
+    result = p.decode_message(msg)
+    assert result['type'] == 'G'
+    assert result['timestamp'] == "01:01:2024:12:00:00:000"
+    assert result['message_id'] == "abcdefgh12345678"
+    assert result['iid_list'] == [[1, 1]]
+    assert result['value_list'] == []
+    assert result['error_list'] == []
+
+def test_valid_set_with_int():
+    p = Protocol()
+    msg = TAG + msg_type_set + timestamp + message_id + iid_section + value_valid_i + error_list_empty
+    result = p.decode_message(msg)
+    assert result['type'] == 'S'
+    assert result['value_list'][0][0] == 'I'
+
+def test_valid_set_with_string():
+    p = Protocol()
+    msg = TAG + msg_type_set + timestamp + message_id + iid_section + value_valid_s + error_list_empty
+    result = p.decode_message(msg)
+    assert result['value_list'][0][0] == 'S'
+
+def test_valid_set_with_timestamp():
+    p = Protocol()
+    msg = TAG + msg_type_set + timestamp + message_id + iid_section + value_valid_t + error_list_empty
+    result = p.decode_message(msg)
+    assert result['value_list'][0][0] == 'T'
+
+def test_invalid_tag():
+    p = Protocol()
+    msg = INVALID_TAG + msg_type_get + timestamp + message_id + iid_section + value_empty + error_list_empty
+    try:
+        p.decode_message(msg)
+        assert False, "Should raise InvalidTagError"
+    except InvalidTagError as e:
+        assert "Invalid message tag." == str(e)
+        pass  # Expected behavior
+
+def test_invalid_message_type():
+    p = Protocol()
+    msg = TAG + invalid_msg_type + timestamp + message_id + iid_section + value_empty + error_list_empty
+    try:
+        p.decode_message(msg)
+        assert False, "Should raise UnknownMessageTypeError"
+    except UnknownMessageTypeError as e:
+        assert "Invalid message type" in str(e)
+        pass  # Expected behavior
+
+def test_invalid_timestamp_type():
+    p = Protocol()
+    msg = TAG + msg_type_get + invalid_timestamp_type + message_id + iid_section + value_empty + error_list_empty
+    try:
+        p.decode_message(msg)
+        assert False, "Should raise DecodingError"
+    except DecodingError as e:
+        assert "Expected timestamp type 'T'" in str(e)
+        pass  # Expected behavior
+
+def test_invalid_timestamp_size():
+    p = Protocol()
+    msg = TAG + msg_type_get + invalid_timestamp_size + message_id + iid_section + value_empty + error_list_empty
+    try:
+        p.decode_message(msg)
+        assert False, "Should raise DecodingError"
+    except DecodingError as e:
+        assert "Invalid timestamp length" in str(e)
+        pass  # Expected behavior
+
+def test_invalid_timestamp_component():
+    p = Protocol()
+    msg = TAG + msg_type_get + invalid_timestamp_component + message_id + iid_section + value_empty + error_list_empty
+    try:
+        p.decode_message(msg)
+        assert False, "Should raise DecodingError"
+    except DecodingError as e:
+        assert "Invalid timestamp component format" in str(e)
+        pass  # Expected behavior
+
+def test_invalid_timestamp_date_format():
+    p = Protocol()
+    msg = TAG + msg_type_get + invalid_timestamp_date_format + message_id + iid_section + value_empty + error_list_empty
+    try:
+        p.decode_message(msg)
+        assert False, "Should raise DecodingError"
+    except DecodingError as e:
+        assert "Invalid timestamp format." == str(e)
+        pass  # Expected behavior
+
+def test_invalid_message_id():
+    p = Protocol()
+    msg = TAG + msg_type_get + timestamp + invalid_msg_id + iid_section + value_empty + error_list_empty
+    try:
+        p.decode_message(msg)
+        assert False, "Should raise DecodingError"
+    except DecodingError as e:
+        assert "Invalid message ID length. Expected 16 characters." == str(e)
+        pass  # Expected behavior
+
+def test_invalid_iid_section_size():
+    p = Protocol()
+    msg = TAG + msg_type_get + timestamp + message_id + invalid_iid_section_size + value_empty + error_list_empty
+    try:
+        p.decode_message(msg)
+        assert False, "Should raise DecodingError"
+    except DecodingError as e:
+        assert "Invalid IID List length format." == str(e)
+        pass  # Expected behavior
+
+def test_invalid_iid_section_type():
+    p = Protocol()
+    msg = TAG + msg_type_get + timestamp + message_id + invalid_iid_section_component_type + value_empty + error_list_empty
+    try:
+        p.decode_message(msg)
+        assert False, "Should raise DecodingError"
+    except DecodingError as e:
+        assert "Expected IID type 'D'" in str(e)
+        pass  # Expected behavior
+
+def test_invalid_iid_section_size():
+    p = Protocol()
+    msg = TAG + msg_type_get + timestamp + message_id + invalid_iid_section_component_size + value_empty + error_list_empty
+    try:
+        p.decode_message(msg)
+        assert False, "Should raise DecodingError"
+    except DecodingError as e:
+        assert "Invalid IID length." in str(e)
+        pass  # Expected behavior
+
+def test_invalid_iid_section_value():
+    p = Protocol()
+    msg = TAG + msg_type_get + timestamp + message_id + invalid_iid_section_component_value + value_empty + error_list_empty
+    try:
+        p.decode_message(msg)
+        assert False, "Should raise DecodingError"
+    except DecodingError as e:
+        assert "Invalid IID component format." in str(e)
+        pass  # Expected behavior
+
+def test_invalid_value_size():
+    p = Protocol()
+    msg = TAG + msg_type_get + timestamp + message_id + iid_section + invalid_value_size + error_list_empty
+    try:
+        p.decode_message(msg)
+        assert False, "Should raise DecodingError"
+    except DecodingError as e:
+        assert "Invalid Value List length format." == str(e)
+        pass  # Expected behavior
+
+def test_invalid_error_list_size():
+    p = Protocol()
+    msg = TAG + msg_type_get + timestamp + message_id + iid_section + value_empty + invalid_error_list_size
+    try:
+        p.decode_message(msg)
+        assert False, "Should raise DecodingError"
+
+    except DecodingError as e:
+        assert "Invalid Error List length format." == str(e)
+        pass  # Expected behavior
+
+def test_unsupported_error_list_size():
+    p = Protocol()
+    msg = TAG + msg_type_get + timestamp + message_id + iid_section + value_empty + unsupported_error_list_size
+    try:
+        p.decode_message(msg)
+        assert False, "Should raise DecodingError"
+    except DecodingError as e:
+        assert "Error List should be empty" in str(e)
+        pass  # Expected behavior
 
 ######################
 # TESTES PARA GET
 ######################
 
-def test_valid_get():
-    p = Protocol()
-    msg = make_message(msg_type_get, value_empty)
-    result = p.decode_message(msg)
-    assert result['type'] == 'G'
-
 def test_get_with_value_should_fail():
     p = Protocol()
-    msg = make_message(msg_type_get, value_valid_i)
+    msg = TAG + msg_type_get + timestamp + message_id + iid_section + value_valid_i + error_list_empty
     try:
         p.decode_message(msg)
         assert False
     except DecodingError as e:
-        assert "should not contain values" in str(e)
+        assert "Get request should not contain values." == str(e)
+
 
 def test_get_with_error_list_not_empty():
     p = Protocol()
@@ -114,56 +327,87 @@ def test_get_with_error_list_not_empty():
     except DecodingError as e:
         assert "Error List should be empty" in str(e)
 
-
 ######################
 # TESTES PARA SET
 ######################
 
-def test_valid_set_with_int():
-    p = Protocol()
-    msg = make_message(msg_type_set, value_valid_i)
-    result = p.decode_message(msg)
-    assert result['type'] == 'S'
-    assert result['value_list'][0][0] == 'I'
 
-def test_valid_set_with_string():
+def test_set_invalid_value_size():
     p = Protocol()
-    msg = make_message(msg_type_set, value_valid_s)
-    result = p.decode_message(msg)
-    assert result['value_list'][0][0] == 'S'
-
-def test_valid_set_with_timestamp():
-    p = Protocol()
-    msg = make_message(msg_type_set, value_valid_t)
-    result = p.decode_message(msg)
-    assert result['value_list'][0][0] == 'T'
-
-def test_set_mismatched_values():
-    p = Protocol()
-    bad_value = b"2\0" + b"I\0" + b"1\0" + b"5\0" + b"I\0" + b"1\0" + b"6\0"
-    msg = make_message(msg_type_set, bad_value)
+    msg = TAG + msg_type_set + timestamp + message_id + iid_section + invalid_value_more_than_one + error_list_empty
     try:
         p.decode_message(msg)
-        assert False
-    except IIDValueMismatchError:
-        pass
+        assert False, "Should raise IIDValueMismatchError"
+    except IIDValueMismatchError as e:
+        assert "Number of values does not match number of IIDs." == str(e)
+        pass  # Expected behavior
 
-def test_set_invalid_value_type():
+def test_invalid_value_type():
     p = Protocol()
-    bad_value = b"1\0" + b"X\0" + b"1\0" + b"9\0"
-    msg = make_message(msg_type_set, bad_value)
+    msg = TAG + msg_type_set + timestamp + message_id + iid_section + invalid_value_component_type + error_list_empty
     try:
         p.decode_message(msg)
-        assert False
-    except InvalidValueTypeError:
-        pass
+        assert False, "Should raise InvalidValueTypeError"
+    except InvalidValueTypeError as e:
+        assert "Unsupported value type 'D' in value list for message type S." == str(e)
+        pass  # Expected behavior
 
-def test_set_invalid_iid_component():
+def test_invalid_value_component_size():
     p = Protocol()
-    bad_iid = b"1\0" + b"D\0" + b"2\0" + b"x\0" + b"1\0"
-    msg = TAG + msg_type_set + timestamp + message_id + bad_iid + value_valid_i + error_list_empty
+    msg = TAG + msg_type_set + timestamp + message_id + iid_section + invalid_value_component_size + error_list_empty
     try:
         p.decode_message(msg)
-        assert False
+        assert False, "Should raise DecodingError"
     except DecodingError as e:
-        assert "Invalid IID component format" in str(e)
+        assert "Invalid value length format." in str(e)
+        pass  # Expected behavior
+
+def test_invalid_value_i_component_size():
+    p = Protocol()
+    msg = TAG + msg_type_set + timestamp + message_id + iid_section + invalid_value_i_componente_size + error_list_empty
+    try:
+        p.decode_message(msg)
+        assert False, "Should raise DecodingError"
+    except DecodingError as e:
+        assert "Type I must have length 1." in str(e)
+        pass  # Expected behavior
+
+def test_invalid_value_s_component_size():
+    p = Protocol()
+    msg = TAG + msg_type_set + timestamp + message_id + iid_section + invalid_value_s_componente_size + error_list_empty
+    try:
+        p.decode_message(msg)
+        assert False, "Should raise DecodingError"
+    except DecodingError as e:
+        assert "Type S must have length 1." in str(e)
+        pass  # Expected behavior
+
+def test_invalid_value_t_component_size():
+    p = Protocol()
+    msg = TAG + msg_type_set + timestamp + message_id + iid_section + invalid_value_t_componente_size + error_list_empty
+    try:
+        p.decode_message(msg)
+        assert False, "Should raise DecodingError"
+    except DecodingError as e:
+        assert "Invalid timestamp length for S messages." in str(e)
+        pass  # Expected behavior
+
+def test_invalid_value_i_component_value():
+    p = Protocol()
+    msg = TAG + msg_type_set + timestamp + message_id + iid_section + invalid_value_component_value + error_list_empty
+    try:
+        p.decode_message(msg)
+        assert False, "Should raise DecodingError"
+    except DecodingError as e:
+        assert "Invalid value format." in str(e)
+        pass  # Expected behavior
+
+def test_invalid_value_t_component_value():
+    p = Protocol()
+    msg = TAG + msg_type_set + timestamp + message_id + iid_section + invalid_value_component_t_value + error_list_empty
+    try:
+        p.decode_message(msg)
+        assert False, "Should raise DecodingError"
+    except DecodingError as e:
+        assert "Invalid date timestamp format." in str(e)
+        pass  # Expected behavior
